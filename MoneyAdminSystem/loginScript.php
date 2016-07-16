b03d080285129252c89b6849d8fcb884584174e5
Yuduki <Developer.Yuduki@gmail.com>
2016-07-16 10:42:53 +0900

    J

--- MoneyAdminSystem/loginScript.php
+++ MoneyAdminSystem/loginScript.php
@@ -1,10 +1,7 @@
 <?php
-  error_reporting(E_ALL | E_STRICT);
-  ini_set('display_errors', 'On');
 $dsn = 'mysql:dbname=ID_Pass; host=localhost; charset=utf8';
 $usr = 'Yuduki';
 $passwd = 'dev';
-$pdo;
     try{
       $pdo = new pdo ($dsn, $usr , $passwd);
     }catch(PDOException $e){
@@ -13,7 +10,7 @@ $pdo;
 function username_authorization(){
   $name = 0;
   $name = $POST['username'];
-  $sql = "select * from ID_Pass where ID='$name'";
+  $sql = "select * from ID_Pass where ID='".$name."'";
   $statement = $pdo->query($sql);
 }
   username_authorization();
